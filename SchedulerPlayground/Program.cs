86a6d5fcad6d539c1188551f9757e5a459323366
Enrico Massone <enrico.massone@deltatre.it>
2018-02-20 12:37:11 +0100

    Added an example to restart the scheduler

--- SchedulerPlayground/Program.cs
+++ SchedulerPlayground/Program.cs
@@ -13,22 +13,41 @@ namespace SchedulerPlayground
 	{
 		private static void Main(string[] args)
 		{
-			var scheduler = new Scheduler(WaitFiveSecondsAsync);
-			WriteLine("Welcome to webplu scheduler playground. Press a key to start.");
+			var scheduler = new Scheduler(WaitThreeSecondsAsync);
+			WriteLine("Welcome to webplu scheduler playground. Press enter to start.");
 			ReadLine();
 
-			scheduler.Start(2000, 1000);
-			WriteLine("Scheduler has been started. Press a key to stop the stop the scheduler.");
+			scheduler.Start(1000, 1000);
+			WriteLine("Scheduler has been started. Press enter to stop it.");
 			WriteLine();
 
 			ReadLine();
-			scheduler.Dispose();
+
+			scheduler.Stop();
+			WriteLine("Press 'R' key to restart the timer or anything else to close the program.");
+
+			var key = ReadKey();
+			if (key.Key == ConsoleKey.R)
+			{
+				scheduler.Restart(2000, 1000);
+
+				WriteLine("\nScheduler has been restarted. Press enter to stop it.");
+				WriteLine();
+
+				ReadLine();
+
+				scheduler.Dispose();
+			}
+			else
+			{
+				scheduler.Dispose();
+			}
 		}
 
-		private static async Task WaitFiveSecondsAsync(CancellationToken token)
+		private static async Task WaitThreeSecondsAsync(CancellationToken token)
 		{
 			WriteLine($"Start awaiting delay. {DateTime.Now.ToString(CultureInfo.InvariantCulture)}");
-			await Task.Delay(5000, token).ConfigureAwait(false);
+			await Task.Delay(3000, token).ConfigureAwait(false);
 			WriteLine($"Delay expired. {DateTime.Now.ToString(CultureInfo.InvariantCulture)}");
 			WriteLine();
 		}
