afece75236ae19c4f3957e90f39fd4bba1b46f39
Enrico Massone <enrico.massone@deltatre.it>
2018-02-20 12:05:02 +0100

    Added extension method and scheduler class

---
+++ SchedulerPlayground/Extensions/SchedulerExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SchedulerPlayground.Extensions
+{
+	public static class SchedulerExtensions
+	{
+		/// <exception cref="ArgumentNullException">Throws when parameter scheduler is null</exception>
+		/// <exception cref="ArgumentNullException">Throws when parameter actionToBeScheduled is null</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Throws when parameter dueTimeMs is negative and other than -1</exception>
+		/// <exception cref="ArgumentOutOfRangeException">Throws when parameter periodMs is negative and other than -1</exception>
+		public static IDisposable ScheduleRecurringAction(
+			this IScheduler scheduler,
+			int dueTimeMs,
+			int periodMs,
+			Func<CancellationToken, Task> asyncAction,
+			CancellationToken token = default(CancellationToken),
+			Action<Exception> onError = null)
+		{
+			if (scheduler == null)
+				throw new ArgumentNullException(nameof(scheduler));
+
+			if (asyncAction == null)
+				throw new ArgumentNullException(nameof(asyncAction));
+
+			if (!IsValidTimeInterval(dueTimeMs))
+				throw new ArgumentOutOfRangeException(nameof(dueTimeMs), $"Invalid time interval in milliseconds: {dueTimeMs}");
+
+			if (!IsValidTimeInterval(periodMs))
+				throw new ArgumentOutOfRangeException(nameof(periodMs), $"Invalid time interval in milliseconds: {periodMs}");
+
+			var dueTimeTimeSpan = TimeSpan.FromMilliseconds(dueTimeMs);
+			var periodTimeTimeSpan = TimeSpan.FromMilliseconds(periodMs);
+			var errorHandler = onError ?? TraceError;
+
+			if (dueTimeMs == Timeout.Infinite)
+			{
+				return scheduler.Schedule(TimeSpan.FromMilliseconds(0), _ => { });
+			}
+
+			return scheduler.Schedule(dueTimeTimeSpan, async scheduleNext =>
+			{
+				try
+				{
+					await asyncAction(token).ConfigureAwait(false);
+				}
+				catch (OperationCanceledException)
+				{
+					//Cancellation of async action has been requested from outside
+				}
+				catch (Exception exception)
+				{
+					errorHandler(exception);
+				}
+
+				if (periodMs != Timeout.Infinite)
+				{
+					scheduleNext(periodTimeTimeSpan);
+				}
+			});
+
+			void TraceError(Exception exception) =>
+				Trace.TraceError($"An error occurred while running scheduled action: {exception}");
+
+			bool IsValidTimeInterval(int milliseconds) =>
+				milliseconds >= 0 || milliseconds == Timeout.Infinite;
+		}
+	}
+}
---
+++ SchedulerPlayground/Scheduler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SchedulerPlayground.Extensions;
+
+namespace SchedulerPlayground
+{
+	public sealed class AsyncScheduler : IDisposable
+	{
+		private static readonly IScheduler Scheduler = System.Reactive.Concurrency.Scheduler.Default;
+
+		private readonly Func<CancellationToken, Task> _asyncAction;
+		private readonly object _lock;
+		private IDisposable _schedule;
+		private CancellationTokenSource _cancellation;
+		private bool _isRunning;
+
+		/// <param name="asyncAction">The asynchronous and cancellable action to be scheduled for execution</param>
+		/// <exception cref="System.ArgumentNullException">Throws when parameter asyncAction is null</exception>
+		public AsyncScheduler(Func<CancellationToken, Task> asyncAction)
+		{
+			_asyncAction = asyncAction ?? throw new ArgumentNullException(nameof(asyncAction));
+			_schedule = null;
+			_lock = new object();
+			_cancellation = null;
+			_isRunning = false;
+		}
+
+		/// <summary>
+		/// Call this method to start the scheduler. If an attempt is made to start the scheduler when it is already running nothing will happen.
+		/// </summary>
+		/// <param name="dueTimeMs">The amount of time to delay before invoking the callback for the first time, in milliseconds. Specify Timeout.Infinite to prevent the scheduler from restarting. Specify zero (0) to start the scheduler immediately.</param>
+		/// <param name="periodMs">The time interval between two consecutives invocations of the callback. Specify Timeout.Infinite to disable periodic signaling.</param>
+		public void Start(int dueTimeMs, int periodMs)
+		{
+			lock (_lock)
+			{
+				StartInner(dueTimeMs, periodMs);
+			}
+		}
+
+		private void StartInner(int dueTimeMs, int periodMs)
+		{
+			if (_isRunning)
+			{
+				return;
+			}
+
+			_cancellation = new CancellationTokenSource();
+
+			_schedule = Scheduler.ScheduleRecurringAction(
+				dueTimeMs,
+				periodMs,
+				_asyncAction,
+				_cancellation.Token);
+
+			_isRunning = true;
+		}
+
+		/// <summary>
+		/// Call this method to stop the scheduler. If an attempt is made to stop the scheduler when it is not running nothing will happen.
+		/// </summary>
+		public void Stop()
+		{
+			lock (_lock)
+			{
+				StopInner();
+			}
+		}
+
+		private void StopInner()
+		{
+			if (!_isRunning)
+			{
+				return;
+			}
+
+			_schedule?.Dispose();
+			_schedule = null;
+
+			_cancellation?.Cancel();
+			_cancellation?.Dispose();
+			_cancellation = null;
+
+			_isRunning = false;
+		}
+
+		/// <summary>
+		/// Call this method to restart a running scheduler or to start a not running scheduler.
+		/// </summary>
+		/// <param name="dueTimeMs">The amount of time to delay before invoking the callback for the first time, in milliseconds. Specify Timeout.Infinite to prevent the scheduler from restarting. Specify zero (0) to start the scheduler immediately.</param>
+		/// <param name="periodMs">The time interval between two consecutives invocations of the callback. Specify Timeout.Infinite to disable periodic signaling.</param>
+		public void Restart(int dueTimeMs, int periodMs)
+		{
+			lock (_lock)
+			{
+				if (_isRunning)
+				{
+					StopInner();
+				}
+
+				StartInner(dueTimeMs, periodMs);
+			}
+		}
+
+		public void Dispose() => Stop();
+	}
+}
